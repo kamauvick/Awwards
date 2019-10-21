d00b0cfbea585fe322de11167dc16432e396e24e
kamauvick <kamauvick@outlook.com>
2019-10-21 20:27:54 +0300

    Add a home view to display the users projects.
    extend the base template file.

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import *
+
+
+def home(request):
+    current_user = request.user
+    project_images = Project.fetch_all_images()
+    params = {
+        'all_images': project_images,
+        'current_user': current_user,
+    }
+    return render(request, "main/home.html", params)
