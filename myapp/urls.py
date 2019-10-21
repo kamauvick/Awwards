a6133f8aa8d4f8372f9f21c8fe04cda8e67880f7
kamauvick <kamauvick@outlook.com>
2019-10-21 20:36:12 +0300

    Update the application paths with a url for home.

---
+++ myapp/urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import re_path
+
+from .views import home
+
+urlpatterns = [
+    re_path(r'^home$', home, name='home')
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
