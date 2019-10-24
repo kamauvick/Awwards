1f50fbb9d828fb953f9f882a53ae22104fc654f5
kamauvick <kamauvick@outlook.com>
2019-10-24 12:00:20 +0300

    Update the models file with a filed for creativity ratings.

--- myapp/models.py
+++ myapp/models.py
@@ -24,6 +24,10 @@ class Profile(models.Model):
         self.bio = bio
         self.save()
 
+    @classmethod
+    def get_profile_data(cls):
+        return Profile.objects.all()
+
     class Meta:
         db_table = 'profiles'
 
@@ -37,6 +41,7 @@ class Project(models.Model):
     user_project_id = models.IntegerField(default=0)
     design = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
     usability = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
+    creativity = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
     content = models.IntegerField(choices=list(zip(range(0, 11), range(0, 11))), default=0)
     vote_submissions = models.IntegerField(default=0)
 
@@ -83,7 +88,7 @@ class Comment(models.Model):
 
 class Rate(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
-    post = models.ForeignKey(Project, on_delete=models.PROTECT, related_name='likes')
+    post = models.ForeignKey(Project, on_delete=models.PROTECT, related_name='likes', null=True)
     design = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
     usability = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)], null=True)
     creativity = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
